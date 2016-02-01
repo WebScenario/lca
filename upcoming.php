6e951cb38e8625ead6f09bebdd7cc11cce4244b3
Wally <walther.graciano@gmail.com>
2016-02-01 00:24:45 -0300

    Logo upcoming

---
+++ upcoming.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Los Cuatro Ases</title>
+  <link rel="stylesheet" href="css/styles.css">
+
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+  <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+
+</head>
+<body>
+  <?php include 'php/logo.php'; ?>
+</body>
+</html>
