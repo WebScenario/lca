d0d6baae4756a9a36f89bc14786db5c891906237
Wally <walther.graciano@gmail.com>
2016-06-17 18:47:29 -0300

    LCA V2

---
+++ lca_v2/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () { 
+  // $('.navbar-brand>span').hover(function(){
+  //   $(this).addClass('animated fadeInUp');
+  // }, function() {
+  //   $(this).removeClass('animated fadeInUp');
+  // });
+  // $('ul.nav.navbar-nav.navbar-right>li>a').on('click',function() {
+  // console.log('hello');
+    // $('.navbar-underline').removeClass($('.navbar-underline').attr('class').split(' ')[1]).addClass($(this).closest('li').attr('class')+'-active');
+  //     $('.navbar-underline').attr('class').split(' ')[1]).addClass(
+      // console.log($(this).closest('li').attr('class')+'-active');
+  // });
+});
